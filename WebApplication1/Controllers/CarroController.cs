7bc754fbd1e83f5355ca0f80457a7072b4800d8e
vvilas <vitor.vboas@gmail.com>
2017-09-04 20:48:11 -0300

    Hello MVC

---
+++ WebApplication1/Controllers/CarroController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarroController : Controller
+    {
+        // GET: Carro
+        public ActionResult Index()
+        {
+            var model = _carros.OrderBy(m => m.Id);
+            return View(model);
+        }
+
+        public ActionResult Edit(int Id) {
+
+            var carro = _carros.Single(m => m.Id == Id);
+
+            return View(carro);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CarroModel carro) {
+
+            _carros.Remove(_carros.Single(m => m.Id == carro.Id));
+
+            _carros.Add(carro);
+
+            return RedirectToAction("Index");
+        }
+
+        static List<CarroModel> _carros = new List<CarroModel> {
+            new CarroModel {Id=1, Modelo="Gol",Ano = 1996,Placa="EYN-1609" },
+            new CarroModel {Id=2, Modelo="Civic",Ano = 2016,Placa="GEN-1969" },
+            new CarroModel {Id=3, Modelo="Uno",Ano = 1994,Placa="AEN-1597" },
+            new CarroModel {Id=4, Modelo="Prisma",Ano = 2014,Placa="CUI-9856" }
+        };
+    }
+}
---
+++ WebApplication1/Models/CarroModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CarroModel
+    {
+        public int Id { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Placa{ get; set; }
+
+    }
+}
